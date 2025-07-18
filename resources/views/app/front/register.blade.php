f69426b948c529cf6b1d4c20555a3324f65a0e1a
AbelBravila <abelbravila0@gmail.com>
2025-07-17 21:14:38 -0600

    otra pruebba

--- resources/views/app/front/register.blade.php
+++ resources/views/app/front/register.blade.php
@@ -1,5 +1,5 @@
 <x-apartados-layout>
-<!-- Quote Start -->
+    <!-- Quote Start -->
     <div class="container-fluid py-5">
         <div class="container">
             <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
@@ -11,28 +11,33 @@
                         <div class="row g-3">
                             <div class="col-sm-6">
                                 <div class="form-floating">
-                                    <input type="text" class="form-control border-0" id="gname" placeholder="Gurdian Name">
-                                    <label for="gname">Your Name</label>
+                                    <input type="text" class="form-control border-0" id="gname"
+                                        placeholder="Gurdian Name">
+                                    <label for="gname">First Name</label>
                                 </div>
                             </div>
                             <div class="col-sm-6">
                                 <div class="form-floating">
-                                    <input type="email" class="form-control border-0" id="gmail" placeholder="Gurdian Email">
+                                    <input type="text" class="form-control border-0" id="cage"
+                                        placeholder="Child Age">
+                                    <label for="cage">Last Name</label>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-floating">
+                                    <input type="email" class="form-control border-0" id="gmail"
+                                        placeholder="Gurdian Email">
                                     <label for="gmail">Your Email</label>
                                 </div>
                             </div>
                             <div class="col-sm-6">
                                 <div class="form-floating">
-                                    <input type="text" class="form-control border-0" id="cname" placeholder="Child Name">
+                                    <input type="text" class="form-control border-0" id="cname"
+                                        placeholder="Child Name">
                                     <label for="cname">Your Mobile</label>
                                 </div>
                             </div>
-                            <div class="col-sm-6">
-                                <div class="form-floating">
-                                    <input type="text" class="form-control border-0" id="cage" placeholder="Child Age">
-                                    <label for="cage">Service Type</label>
-                                </div>
-                            </div>
+
 
                             <div class="col-12 text-center">
                                 <button class="btn btn-primary py-3 px-4" type="submit">Create Account</button>
